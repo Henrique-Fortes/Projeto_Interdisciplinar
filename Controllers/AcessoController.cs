ed299c89fdcbee5284cbf38b7853642e1a1995ba
Henrique H. da Silva Fortes <fortes_henrique@hotmail.com>
2017-06-07 03:54:57 -0300

    Add files via upload

---
+++ Controllers/AcessoController.cs
@@ -0,0 +1,129 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Controle_Acesso.Models;
+
+namespace Controle_Acesso.Controllers
+{
+    public class AcessoController : Controller
+    {
+        private DEVMEDIAEntities db = new DEVMEDIAEntities();
+
+        // GET: Acesso
+        public ActionResult Index()
+        {
+            return View(db.ACESSO.ToList());
+        }
+
+        // GET: Acesso/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACESSO aCESSO = db.ACESSO.Find(id);
+            if (aCESSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCESSO);
+        }
+
+        // GET: Acesso/Create
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Acesso/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_LOGIN,EMAIL,SENHA,ATIVO,PERFIL,NOME,SOBRENOME")] ACESSO aCESSO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ACESSO.Add(aCESSO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aCESSO);
+        }
+
+        // GET: Acesso/Edit/5
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACESSO aCESSO = db.ACESSO.Find(id);
+            if (aCESSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCESSO);
+        }
+
+        // POST: Acesso/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_LOGIN,EMAIL,SENHA,ATIVO,PERFIL,NOME,SOBRENOME")] ACESSO aCESSO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aCESSO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aCESSO);
+        }
+
+        // GET: Acesso/Delete/5
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ACESSO aCESSO = db.ACESSO.Find(id);
+            if (aCESSO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aCESSO);
+        }
+
+        // POST: Acesso/Delete/5
+        [Authorize(Roles = "Administrador")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ACESSO aCESSO = db.ACESSO.Find(id);
+            db.ACESSO.Remove(aCESSO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Controle_Acesso.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
